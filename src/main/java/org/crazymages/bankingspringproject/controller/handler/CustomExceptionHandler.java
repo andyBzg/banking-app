f6fb6dd04d4d3ae6d600223c37f30f873cfd39f1
andyBzg <andr.bzg@gmail.com>
2023-06-19 22:31:36 +0200

    feat: add handler for InsufficientFundsException and TransactionNotAllowedException

--- src/main/java/org/crazymages/bankingspringproject/controller/handler/CustomExceptionHandler.java
+++ src/main/java/org/crazymages/bankingspringproject/controller/handler/CustomExceptionHandler.java
@@ -1,7 +1,10 @@
 package org.crazymages.bankingspringproject.controller.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.crazymages.bankingspringproject.exception.TransactionNotAllowedException;
 import org.crazymages.bankingspringproject.exception.DataNotFoundException;
+import org.crazymages.bankingspringproject.exception.InsufficientFundsException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +15,20 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<String> handleDataNotFoundException(Exception e) {
-        log.error("Entity with uuid {} not found", e.getMessage());
+        log.error("Entity with id {} not found", e.getMessage());
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(InsufficientFundsException.class)
+    public ResponseEntity<String> handleInsufficientFundsExceptionException(Exception e) {
+        log.error("Insufficient funds in sender's account id {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    @ExceptionHandler(TransactionNotAllowedException.class)
+    public ResponseEntity<String> handleTransactionNotAllowedException() {
+        log.error("Not allowed to execute transaction");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
 }
