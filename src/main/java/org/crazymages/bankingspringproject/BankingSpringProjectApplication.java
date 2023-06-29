0dd36fc1fdc6dc504db441f965db9d8c2deb3808
andyBzg <andr.bzg@gmail.com>
2023-06-29 19:22:06 +0200

    feat: add scheduling annotation

--- src/main/java/org/crazymages/bankingspringproject/BankingSpringProjectApplication.java
+++ src/main/java/org/crazymages/bankingspringproject/BankingSpringProjectApplication.java
@@ -2,8 +2,10 @@ package org.crazymages.bankingspringproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BankingSpringProjectApplication {
 
     public static void main(String[] args) {
