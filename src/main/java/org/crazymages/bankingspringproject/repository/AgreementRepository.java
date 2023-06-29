5d6ac7224e37fe4a5192ef07393b6dcb999a4690
andyBzg <andr.bzg@gmail.com>
2023-06-29 18:54:46 +0200

    feat: add method and query to find agreement by client uuid and certain ProductType

--- src/main/java/org/crazymages/bankingspringproject/repository/AgreementRepository.java
+++ src/main/java/org/crazymages/bankingspringproject/repository/AgreementRepository.java
@@ -1,12 +1,15 @@
 package org.crazymages.bankingspringproject.repository;
 
 import org.crazymages.bankingspringproject.entity.Agreement;
+import org.crazymages.bankingspringproject.entity.enums.AgreementStatus;
+import org.crazymages.bankingspringproject.entity.enums.ProductType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -24,4 +27,19 @@ public interface AgreementRepository extends JpaRepository<Agreement, UUID> {
             "WHERE cl.uuid = :clientUuid")
     List<Agreement> findAgreementsWhereClientIdIs(@Param("clientUuid") UUID clientUuid);
 
+    @Query("SELECT ag FROM Agreement ag " +
+            "JOIN Account ac ON ac.uuid = ag.accountUuid " +
+            "JOIN Client cl ON cl.uuid = ac.clientUuid " +
+            "JOIN Product pr ON pr.uuid = ag.productUuid " +
+            "WHERE cl.uuid = :clientUuid " +
+            "AND pr.type = :type")
+    Optional<Agreement> findAgreementByClientIdAndProductType(@Param("clientUuid") UUID clientUuid, @Param("type")ProductType type);
+
+//    @Query("SELECT ag FROM Agreement ag " +
+//            "JOIN Product pr ON pr.uuid = ag.productUuid " +
+//            "WHERE ag.status = :status " +
+//            "AND pr.type = :type")
+//    List<Agreement> findAgreementsWhereStatusIsAndProductTypeIs(@Param("status")AgreementStatus status, @Param("type")ProductType type);
+
+//    Agreement findAgreementWhereProductTypeIs(UUID uuid);
 }
