86aefeeda453aedd0aa93a834888e4065cba61e3
andyBzg <andr.bzg@gmail.com>
2023-06-29 18:10:05 +0200

    initial commit

---
+++ src/main/java/org/crazymages/bankingspringproject/entity/enums/ProductType.java
@@ -0,0 +1,11 @@
+package org.crazymages.bankingspringproject.entity.enums;
+
+public enum ProductType {
+
+    CURRENT_ACCOUNT,
+    SAVINGS_ACCOUNT,
+    DEPOSIT_ACCOUNT,
+    CREDIT_CARD,
+    PERSONAL_LOAN,
+    INVESTMENT
+}
