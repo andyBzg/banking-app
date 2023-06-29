3259b1386faf4cb48baf59d9ff2f24004e8a5bb8
andyBzg <andr.bzg@gmail.com>
2023-06-29 18:26:06 +0200

    feat: add method to find managers with a certain status, sort by the number of clients in ascending order

--- src/main/java/org/crazymages/bankingspringproject/repository/ManagerRepository.java
+++ src/main/java/org/crazymages/bankingspringproject/repository/ManagerRepository.java
@@ -1,8 +1,10 @@
 package org.crazymages.bankingspringproject.repository;
 
 import org.crazymages.bankingspringproject.entity.Manager;
+import org.crazymages.bankingspringproject.entity.enums.ManagerStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +18,12 @@ public interface ManagerRepository extends JpaRepository<Manager, UUID> {
             "GROUP BY mg " +
             "ORDER BY COUNT(pr) DESC")
     List<Manager> findAllManagersSortedByProductQuantity();
+
+    @Query("SELECT mg FROM Manager mg " +
+            "JOIN Client cl ON cl.managerUuid = mg.uuid " +
+            "WHERE mg.status = :status " +
+            "GROUP BY mg " +
+            "ORDER BY COUNT(cl) ASC")
+    List<Manager> findManagersSortedByClientCountWhereManagerStatusIs(@Param("status") ManagerStatus status);
 
 }
