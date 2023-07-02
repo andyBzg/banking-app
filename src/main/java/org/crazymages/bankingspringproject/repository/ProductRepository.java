3f01e3ce3d2286f7e44d75541766109e1658b9f0
andyBzg <andr.bzg@gmail.com>
2023-07-02 21:46:00 +0200

    feat: add method that finds product by type, status and currency code

--- src/main/java/org/crazymages/bankingspringproject/repository/ProductRepository.java
+++ src/main/java/org/crazymages/bankingspringproject/repository/ProductRepository.java
@@ -1,12 +1,16 @@
 package org.crazymages.bankingspringproject.repository;
 
 import org.crazymages.bankingspringproject.entity.Product;
+import org.crazymages.bankingspringproject.entity.enums.CurrencyCode;
+import org.crazymages.bankingspringproject.entity.enums.ProductStatus;
+import org.crazymages.bankingspringproject.entity.enums.ProductType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -26,5 +30,8 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     @Query("SELECT pr FROM Product pr WHERE pr.isDeleted = true")
     List<Product> findAllDeleted();
+
+    Optional<Product> findProductByTypeIsAndStatusIsAndCurrencyCodeIs(
+            ProductType type, ProductStatus status, CurrencyCode currencyCode);
 
 }
