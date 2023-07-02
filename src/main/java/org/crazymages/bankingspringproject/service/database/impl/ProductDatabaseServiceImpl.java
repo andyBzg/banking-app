0423afb7addb29730a0d84b15a4de39cc82310c8
andyBzg <andr.bzg@gmail.com>
2023-07-02 21:40:33 +0200

    feat: add new logic to create() feat: add method that finds existing entities feat: add method that finds entities flagged as deleted feat: add method that finds product by type, status and currency feat: add ListValidator dependency

--- src/main/java/org/crazymages/bankingspringproject/service/database/impl/ProductDatabaseServiceImpl.java
+++ src/main/java/org/crazymages/bankingspringproject/service/database/impl/ProductDatabaseServiceImpl.java
@@ -2,11 +2,18 @@ package org.crazymages.bankingspringproject.service.database.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.crazymages.bankingspringproject.entity.Manager;
 import org.crazymages.bankingspringproject.entity.Product;
+import org.crazymages.bankingspringproject.entity.enums.CurrencyCode;
+import org.crazymages.bankingspringproject.entity.enums.ManagerStatus;
+import org.crazymages.bankingspringproject.entity.enums.ProductStatus;
+import org.crazymages.bankingspringproject.entity.enums.ProductType;
 import org.crazymages.bankingspringproject.exception.DataNotFoundException;
 import org.crazymages.bankingspringproject.repository.ProductRepository;
+import org.crazymages.bankingspringproject.service.database.ManagerDatabaseService;
 import org.crazymages.bankingspringproject.service.database.ProductDatabaseService;
-import org.crazymages.bankingspringproject.service.database.updater.EntityUpdateService;
+import org.crazymages.bankingspringproject.service.utils.updater.EntityUpdateService;
+import org.crazymages.bankingspringproject.service.utils.validator.ListValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,9 +27,17 @@ public class ProductDatabaseServiceImpl implements ProductDatabaseService {
 
     private final ProductRepository productRepository;
     private final EntityUpdateService<Product> productUpdateService;
+    private final ManagerDatabaseService managerDatabaseService;
+    private final ListValidator<Product> listValidator;
 
     @Override
+    @Transactional
     public void create(Product product) {
+        if (product.getManagerUuid() == null) {
+            List<Manager> activeManagers = managerDatabaseService.findManagersSortedByProductQuantity(ManagerStatus.ACTIVE);
+            Manager firstManager = managerDatabaseService.getFirstManager(activeManagers);
+            product.setManagerUuid(firstManager.getUuid());
+        }
         productRepository.save(product);
         log.info("product created");
     }
@@ -30,7 +45,24 @@ public class ProductDatabaseServiceImpl implements ProductDatabaseService {
     @Override
     public List<Product> findAll() {
         log.info("retrieving list of products");
-        return productRepository.findAll();
+        List<Product> products = productRepository.findAll();
+        return listValidator.validate(products);
+    }
+
+    @Override
+    @Transactional
+    public List<Product> findAllNotDeleted() {
+        log.info("retrieving list of not deleted products");
+        List<Product> products = productRepository.findAllNotDeleted();
+        return listValidator.validate(products);
+    }
+
+    @Override
+    @Transactional
+    public List<Product> findDeletedProducts() {
+        log.info("retrieving list of deleted products");
+        List<Product> deletedProducts = productRepository.findAllDeleted();
+        return listValidator.validate(deletedProducts);
     }
 
     @Override
@@ -38,6 +70,14 @@ public class ProductDatabaseServiceImpl implements ProductDatabaseService {
         log.info("retrieving product by id {}", uuid);
         return productRepository.findById(uuid)
                 .orElseThrow(() -> new DataNotFoundException(String.valueOf(uuid)));
+    }
+
+    @Override
+    public Product findProductByTypeAndStatusAndCurrencyCode(ProductType type, ProductStatus status, CurrencyCode currencyCode) {
+        log.info("retrieving product by type {}", type);
+        return productRepository
+                .findProductByTypeIsAndStatusIsAndCurrencyCodeIs(type, ProductStatus.ACTIVE, currencyCode)
+                .orElseThrow(() -> new DataNotFoundException(String.valueOf(type)));
     }
 
     @Override
