717dcd23498161713c438cd610bad40cfccca738
andyBzg <andr.bzg@gmail.com>
2023-07-02 21:39:41 +0200

    feat: add method that finds existing entities feat: add method that finds entities flagged as deleted feat: add method that finds product by type, status and currency

--- src/main/java/org/crazymages/bankingspringproject/service/database/ProductDatabaseService.java
+++ src/main/java/org/crazymages/bankingspringproject/service/database/ProductDatabaseService.java
@@ -1,6 +1,9 @@
 package org.crazymages.bankingspringproject.service.database;
 
 import org.crazymages.bankingspringproject.entity.Product;
+import org.crazymages.bankingspringproject.entity.enums.CurrencyCode;
+import org.crazymages.bankingspringproject.entity.enums.ProductStatus;
+import org.crazymages.bankingspringproject.entity.enums.ProductType;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,7 +14,13 @@ public interface ProductDatabaseService {
 
     List<Product> findAll();
 
+    List<Product> findAllNotDeleted();
+
+    List<Product> findDeletedProducts();
+
     Product findById(UUID uuid);
+
+    Product findProductByTypeAndStatusAndCurrencyCode(ProductType type, ProductStatus status, CurrencyCode currencyCode);
 
     void update(UUID uuid, Product product);
 
