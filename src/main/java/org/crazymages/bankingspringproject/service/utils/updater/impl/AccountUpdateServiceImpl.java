c9eebf1bc943c34bb3aabc65554820eee8a1ea32
andyBzg <andr.bzg@gmail.com>
2023-07-09 17:22:54 +0200

    feat: refactor for better readability

--- src/main/java/org/crazymages/bankingspringproject/service/utils/updater/impl/AccountUpdateServiceImpl.java
+++ src/main/java/org/crazymages/bankingspringproject/service/utils/updater/impl/AccountUpdateServiceImpl.java
@@ -14,25 +14,30 @@ public class AccountUpdateServiceImpl implements EntityUpdateService<Account> {
     @Override
     public Account update(Account account, Account accountUpdate) {
         if (account != null && accountUpdate != null) {
-            if (accountUpdate.getClientUuid() != null) {
-                account.setClientUuid(accountUpdate.getClientUuid());
-            }
-            if (accountUpdate.getName() != null) {
-                account.setName(accountUpdate.getName());
-            }
-            if (accountUpdate.getType() != null) {
-                account.setType(accountUpdate.getType());
-            }
-            if (accountUpdate.getStatus() != null) {
-                account.setStatus(accountUpdate.getStatus());
-            }
-            if (accountUpdate.getBalance() != null) {
-                account.setBalance(accountUpdate.getBalance());
-            }
-            if (accountUpdate.getCurrencyCode() != null) {
-                account.setCurrencyCode(accountUpdate.getCurrencyCode());
-            }
+            updateProperties(account, accountUpdate);
         }
         return account;
+    }
+
+    @Override
+    public void updateProperties(Account account, Account accountUpdate) {
+        if (accountUpdate.getClientUuid() != null) {
+            account.setClientUuid(accountUpdate.getClientUuid());
+        }
+        if (accountUpdate.getName() != null) {
+            account.setName(accountUpdate.getName());
+        }
+        if (accountUpdate.getType() != null) {
+            account.setType(accountUpdate.getType());
+        }
+        if (accountUpdate.getStatus() != null) {
+            account.setStatus(accountUpdate.getStatus());
+        }
+        if (accountUpdate.getBalance() != null) {
+            account.setBalance(accountUpdate.getBalance());
+        }
+        if (accountUpdate.getCurrencyCode() != null) {
+            account.setCurrencyCode(accountUpdate.getCurrencyCode());
+        }
     }
 }
