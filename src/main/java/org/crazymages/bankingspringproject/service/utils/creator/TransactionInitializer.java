c20199cae95b4d8a42e88235bcb06364db2e4994
andyBzg <andr.bzg@gmail.com>
2023-07-21 11:49:58 +0200

    add argument check and documentation

--- src/main/java/org/crazymages/bankingspringproject/service/utils/creator/TransactionInitializer.java
+++ src/main/java/org/crazymages/bankingspringproject/service/utils/creator/TransactionInitializer.java
@@ -5,18 +5,27 @@ import org.crazymages.bankingspringproject.entity.Transaction;
 import org.springframework.stereotype.Component;
 
 /**
- * A component class that implements the BiFunction interface to create a Transaction object
- * based on the provided accounts information.
+ * A component class responsible for initializing new Transaction entities based on the provided sender and recipient Accounts.
  */
 @Component
 public class TransactionInitializer {
 
+    /**
+     * Initializes a new Transaction entity based on the provided sender and recipient Accounts.
+     *
+     * @param sender    The Account representing the sender of the transaction.
+     * @param recipient The Account representing the recipient of the transaction.
+     * @return The initialized Transaction entity.
+     * @throws IllegalArgumentException if the provided recipient is null.
+     */
     public Transaction initializeTransaction(Account sender, Account recipient) {
+        if (recipient == null) {
+            throw new IllegalArgumentException("recipient is null");
+        }
         Transaction transaction = new Transaction();
         transaction.setDebitAccountUuid(sender.getUuid());
         transaction.setCreditAccountUuid(recipient.getUuid());
         transaction.setCurrencyCode(sender.getCurrencyCode());
-
         return transaction;
     }
 }
