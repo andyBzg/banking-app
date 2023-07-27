a45abc9f9e7ed0a861986003ad885852634fe03a
andyBzg <andr.bzg@gmail.com>
2023-07-27 18:28:05 +0200

    add registration service implementation

---
+++ src/main/java/org/crazymages/bankingspringproject/service/registration/impl/RegistrationServiceImpl.java
@@ -0,0 +1,75 @@
+package org.crazymages.bankingspringproject.service.registration.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.crazymages.bankingspringproject.dto.RegistrationDto;
+import org.crazymages.bankingspringproject.entity.Client;
+import org.crazymages.bankingspringproject.entity.Manager;
+import org.crazymages.bankingspringproject.entity.enums.ClientStatus;
+import org.crazymages.bankingspringproject.entity.enums.ManagerStatus;
+import org.crazymages.bankingspringproject.entity.enums.Roles;
+import org.crazymages.bankingspringproject.exception.UserAlreadyExistsException;
+import org.crazymages.bankingspringproject.repository.ClientRepository;
+import org.crazymages.bankingspringproject.service.database.ManagerDatabaseService;
+import org.crazymages.bankingspringproject.service.registration.RegistrationService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RegistrationServiceImpl implements RegistrationService {
+
+    private final JdbcUserDetailsManager userDetailsManager;
+    private final PasswordEncoder passwordEncoder;
+    private final ManagerDatabaseService managerDatabaseService;
+    private final ClientRepository clientRepository;
+
+    @Override
+    @Transactional
+    public void registerNewClient(RegistrationDto registrationDto) {
+        String email = registrationDto.getEmail();
+        String encodedPassword = passwordEncoder.encode(registrationDto.getPassword());
+
+        if (userDetailsManager.userExists(email)) {
+            throw new UserAlreadyExistsException("Unable to register username, already exists in DB");
+        }
+        saveClientCredentials(email, encodedPassword);
+
+        List<Manager> activeManagers = managerDatabaseService
+                .findManagersSortedByClientQuantityWhereManagerStatusIs(ManagerStatus.ACTIVE);
+        Manager firstManager = managerDatabaseService.getFirstManager(activeManagers);
+
+        Client client = mapRegistrationToClient(registrationDto);
+        client.setManagerUuid(firstManager.getUuid());
+        client.setStatus(ClientStatus.ACTIVE);
+        clientRepository.save(client);
+        log.info("client created");
+    }
+
+    private Client mapRegistrationToClient(RegistrationDto registration) {
+        if (registration == null) {
+            throw new IllegalArgumentException("registrationDto cannot be null");
+        }
+        return Client.builder()
+                .taxCode(registration.getTaxCode())
+                .firstName(registration.getFirstName())
+                .lastName(registration.getLastName())
+                .email(registration.getEmail())
+                .address(registration.getAddress())
+                .phone(registration.getPhone())
+                .build();
+    }
+
+    private void saveClientCredentials(String email, String encodedPassword) {
+        userDetailsManager.createUser(User.withUsername(email)
+                .password(encodedPassword)
+                .roles(Roles.USER.name())
+                .build());
+    }
+}
