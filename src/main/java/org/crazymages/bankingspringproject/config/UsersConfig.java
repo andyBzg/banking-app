6585f8e079f27570b0d652789e5d2500b636b0f8
andyBzg <andr.bzg@gmail.com>
2023-07-27 18:28:49 +0200

    add ability to load managers credentials

--- src/main/java/org/crazymages/bankingspringproject/config/UsersConfig.java
+++ src/main/java/org/crazymages/bankingspringproject/config/UsersConfig.java
@@ -2,33 +2,51 @@ package org.crazymages.bankingspringproject.config;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.crazymages.bankingspringproject.entity.Manager;
 import org.crazymages.bankingspringproject.entity.enums.Roles;
+import org.crazymages.bankingspringproject.repository.ManagerRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor
-public class AdminUserConfig {
+public class UsersConfig {
+
     private final JdbcUserDetailsManager userDetailsManager;
     private final PasswordEncoder passwordEncoder;
+    private final ManagerRepository managerRepository;
 
     @Value("${admin.username}")
     private String adminUsername;
     @Value("${admin.password}")
     private String adminPassword;
+    @Value("${manager.password}")
+    private String managerPassword;
 
     @PostConstruct
-    public void createAdminUser() {
-        String encodedPassword = passwordEncoder.encode(adminPassword);
-
+    public void createUsersAndAuthorities() {
         if (!userDetailsManager.userExists(adminUsername)) {
+            String encodedPassword = passwordEncoder.encode(adminPassword);
             userDetailsManager.createUser(User.withUsername(adminUsername)
                     .password(encodedPassword)
                     .roles(Roles.ADMIN.name(), Roles.MANAGER.name(), Roles.USER.name())
                     .build());
+        }
+
+        List<Manager> managers = managerRepository.findAllNotDeleted();
+        for (Manager manager : managers) {
+            if (!userDetailsManager.userExists(manager.getLastName())) {
+                String encodedManagerPassword = passwordEncoder.encode(managerPassword);
+                userDetailsManager.createUser(User.withUsername(manager.getLastName())
+                        .password(encodedManagerPassword)
+                        .roles(Roles.MANAGER.name(), Roles.USER.name())
+                        .build());
+            }
         }
     }
 }
