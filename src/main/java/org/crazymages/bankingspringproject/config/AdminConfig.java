1322d2effe07c6cdd778025ecebf9ab550c62fac
andyBzg <andr.bzg@gmail.com>
2023-08-06 16:04:27 +0200

    refactor for configure only default admin

--- src/main/java/org/crazymages/bankingspringproject/config/AdminConfig.java
+++ src/main/java/org/crazymages/bankingspringproject/config/AdminConfig.java
@@ -2,31 +2,24 @@ package org.crazymages.bankingspringproject.config;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.crazymages.bankingspringproject.entity.Manager;
 import org.crazymages.bankingspringproject.entity.enums.Roles;
-import org.crazymages.bankingspringproject.service.database.ManagerDatabaseService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
-
 @Configuration
 @RequiredArgsConstructor
-public class UsersConfig {
+public class AdminConfig {
 
     private final JdbcUserDetailsManager userDetailsManager;
     private final PasswordEncoder passwordEncoder;
-    private final ManagerDatabaseService managerDatabaseService;
 
     @Value("${admin.username}")
     private String adminUsername;
     @Value("${admin.password}")
     private String adminPassword;
-    @Value("${manager.password}")
-    private String managerPassword;
 
     @PostConstruct
     public void createUsersAndAuthorities() {
@@ -36,20 +29,6 @@ public class UsersConfig {
                     .password(encodedPassword)
                     .roles(Roles.ADMIN.name(), Roles.MANAGER.name(), Roles.USER.name())
                     .build());
-        }
-
-        List<Manager> managers = managerDatabaseService.findAll()
-                .stream()
-                .filter(m -> !m.isDeleted())
-                .toList();
-        for (Manager manager : managers) {
-            if (!userDetailsManager.userExists(manager.getLastName())) {
-                String encodedManagerPassword = passwordEncoder.encode(managerPassword);
-                userDetailsManager.createUser(User.withUsername(manager.getLastName())
-                        .password(encodedManagerPassword)
-                        .roles(Roles.MANAGER.name(), Roles.USER.name())
-                        .build());
-            }
         }
     }
 }
